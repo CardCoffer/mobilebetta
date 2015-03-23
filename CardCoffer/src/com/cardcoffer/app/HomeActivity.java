58c17c9d289e82e3b23b352f3bf640465893fa8a
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-03-24 03:12:23 +0430

    MainActivity and the corresponding layout was renamed to HomeActivity.

--- CardCoffer/src/com/cardcoffer/app/HomeActivity.java
+++ CardCoffer/src/com/cardcoffer/app/HomeActivity.java
@@ -11,12 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class HomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_home);
 	}
 
 	@Override
