af0218fc99688d98632726bbd8503b4423bb4e85
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-03-26 03:46:28 +0430

    implemented animated search bar. changed the background color.

--- CardCoffer/src/com/cardcoffer/app/HomeActivity.java
+++ CardCoffer/src/com/cardcoffer/app/HomeActivity.java
@@ -2,6 +2,7 @@ package com.cardcoffer.app;
 
 import com.cardcoffer.app.customviews.ItemCardThumbnail;
 
+import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,27 +11,38 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 public class HomeActivity extends Activity {
-	
-	
+
 	LinearLayout llCardContainer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		
+
 		llCardContainer = (LinearLayout) findViewById(R.id.home_llCardContainer);
-		
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main_actionbar, menu);
+
+		// actionbar searchview animation
+
+		SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+		// Get the ID for the search bar LinearLayout
+		int searchBarId = searchView.getContext().getResources().getIdentifier("android:id/search_bar", null, null);
+		// Get the search bar Linearlayout
+		LinearLayout searchBar = (LinearLayout) searchView.findViewById(searchBarId);
+		// Give the Linearlayout a transition animation.
+		searchBar.setLayoutTransition(new LayoutTransition()); //TODO set good animation!
+
 		return true;
 	}
 
@@ -43,12 +55,12 @@ public class HomeActivity extends Activity {
 		if (id == R.id.action_settings) {
 			return true;
 		}
-		if (id == R.id.action_search){
-			
-			for(int i = 0; i<10; i++){
-				
+		if (id == R.id.action_search) {
+
+			for (int i = 0; i < 10; i++) {
+
 				llCardContainer.addView(new ItemCardThumbnail(this));
-				
+
 			}
 			return true;
 		}
